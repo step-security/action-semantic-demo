0c14406e8bd3be6b061c7b708c1bca60e4fd6c0d
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-02-12 17:30:02 +0530

    conflicts resloved

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// This enables ESLint to use dependencies of this config
+// (see https://github.com/eslint/eslint/issues/3458)
+require('eslint-config-molindo/setupPlugins');
+
+module.exports = {
+  extends: 'molindo/javascript',
+  env: {
+    node: true,
+    jest: true,
+    es6: true
+  }
+};
